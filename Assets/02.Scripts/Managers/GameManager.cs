45e2fa24a742cc6ffc48a98449754fa3660dfea0
고윤아 <0205ria@gmail.com>
2025-06-09 15:38:33 +0900

    🐛Fix StageData null bug
    * 임시 코드 추가

--- Assets/02.Scripts/Managers/GameManager.cs
+++ Assets/02.Scripts/Managers/GameManager.cs
@@ -19,6 +19,14 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        UIManager.Instance.Initialize();
         UIManager.Instance.ShowScreen(ScreenType.Main);
+        
+        // TODO: 스테이지 선택 시 연결
+        StageData firstStage = Resources.Load<StageData>("Stages/Stage01");
+        if (firstStage != null)
+        {
+            StageManager.Instance.LoadStage(firstStage);
+        }
     }
 }
